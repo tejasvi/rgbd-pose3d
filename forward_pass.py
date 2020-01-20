e836a8a2f9ebbcf6c3b873042a0ef439ec88d150
tejasvi <45873379+tejasvi@users.noreply.github.com>
2020-01-20 13:53:49 +0000

    fix

--- forward_pass.py
+++ forward_pass.py
@@ -8,6 +8,8 @@ import matplotlib.pyplot as plt
 from PoseNet3D import *
 from utils.Camera import *
 
+from timeit import default_timer as timer
+
 # VALUES YOU MIGHT WANT TO CHANGE
 OPE_DEPTH = 1  # in [1, 5]; Number of stages for the 2D network. Smaller number makes the network faster but less accurate
 VPN_TYPE = 'fast'  # in {'fast', 'default'}; which 3D architecture to use
@@ -31,37 +33,49 @@ if __name__ == '__main__':
 
     NOTE: Runtime scales with the number of people in the scene.
     """
-    # load data
-    color = scipy.misc.imread('./color.png')  # color image
-    color = scipy.misc.imresize(color, (1080, 1920))
-    depth_w = scipy.misc.imread('./depth.png').astype('float32')  # depth map warped into the color frame
+    img = ['color.png', 'color2.png', 'color4.png']
+    dep = ['depth.png', 'depth2.png', 'depth4.png']
 
-    # intrinsic calibration data
-    ratio = np.array([1920.0/512.0, 1080.0/424.0])
-    K = np.array([[3.7132019636619111e+02 * ratio[0], 0.0, 2.5185416982679811e+02 * ratio[0]],
-                   [0.0, 3.7095047063504268e+02 * ratio[1], 2.1463524817996452e+02 * ratio[1]],
-                   [0.0, 0.0, 1.0]])
-    cam = Camera(K)
+    for i in range(3):
 
-    # create algorithm
-    poseNet = PoseNet3D(ope_depth=OPE_DEPTH, vpn_type=VPN_TYPE,
-                        gpu_id=GPU_ID, gpu_memory_limit=GPU_MEMORY, K=K)
+        # load data
+        color = scipy.misc.imread(img[i])  # color image
+        color = scipy.misc.imresize(color, (1080, 1920))
+        depth_w = scipy.misc.imread(dep[i]).astype('float32')  # depth map warped into the color frame
 
-    # loop
-    mask = np.logical_not(depth_w == 0.0)
+        # intrinsic calibration data
+        ratio = np.array([1920.0/512.0, 1080.0/424.0])
+        K = np.array([[3.7132019636619111e+02 * ratio[0], 0.0, 2.5185416982679811e+02 * ratio[0]],
+                       [0.0, 3.7095047063504268e+02 * ratio[1], 2.1463524817996452e+02 * ratio[1]],
+                       [0.0, 0.0, 1.0]])
+        cam = Camera(K)
 
-    # run algorithm
-    coords_pred, det_conf = poseNet.detect(color, depth_w, mask)
+    [14, 0, 3.7132019636619111
+            1392.450736, 0, 944.453137;
+            0, 944.87384, 546.712425;
+            0, 0, 1;
+        # create algorithm
+        poseNet = PoseNet3D(ope_depth=OPE_DEPTH, vpn_type=VPN_TYPE,
+                            gpu_id=GPU_ID, gpu_memory_limit=GPU_MEMORY, K=K)
 
-    # visualize
-    matplotlib.use('Agg')
-    import matplotlib.pyplot as plt
-    fig = plt.figure()
-    ax = fig.add_subplot(111)
-    ax.imshow(color)
-    for i in range(coords_pred.shape[0]):
-        coord2d = cam.project(coords_pred[i, :, :])
-        vis = det_conf[i, :] > CONF_THRESH
-        ax.plot(coord2d[vis, 0], coord2d[vis, 1], 'ro')
-    fig.savefig('test.png')
-    plt.close(fig)
+        # loop
+        mask = np.logical_not(depth_w == 0.0)
+
+        for i in range(10):
+            
+        # run algorithm
+        coords_pred, det_conf = poseNet.detect(color, depth_w, mask)
+
+        # visualize
+        matplotlib.use('Agg')
+        import matplotlib.pyplot as plt
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+        ax.imshow(color)
+        for i in range(coords_pred.shape[0]):
+            coord2d = cam.project(coords_pred[i, :, :])
+            vis = det_conf[i, :] > CONF_THRESH
+            ax.plot(coord2d[vis, 0], coord2d[vis, 1], 'ro')
+        fig.savefig('test.png')
+        plt.close(fig)
+
